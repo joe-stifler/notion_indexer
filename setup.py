d19ae2d53c51bfdb0225873c8a058d54475712d3
Joe Stifler <joserribeiro1017@gmail.com>
2025-01-23 12:00:58 -0300

    build: add setup python files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="notion_indexer",
+    version="0.1.0",
+    author="Joe",
+    author_email="joseribeiro1017@gmail.com",
+    packages=find_packages(include=['notion_indexer']),
+    description="A simple notion indexer.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/joe-stifler/notion_indexer",
+)
